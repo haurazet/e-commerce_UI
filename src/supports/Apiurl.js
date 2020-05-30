8d3639aab3e61a47eaf1da1889fd8ba698e98e85
haura <haura.zahro@gmail.com>
2020-05-31 01:43:35 +0700

    make it better

--- src/supports/Apiurl.js
+++ src/supports/Apiurl.js
@@ -1,2 +1,3 @@
 // export const API_URL='http://localhost:2000'
-export const API_URL='http://localhost:5000'
+// export const API_URL='http://localhost:5000'
+export const API_URL='https://ini-api.herokuapp.com'
